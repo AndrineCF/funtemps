200e85c80a3676c011d48ac44dd7eb8cd9fc15e3
Andrine Celine Flatby <andrinef@stud.ntnu.no>
2023-02-08 20:27:07 +0100

    Laget alle funksjonen for conv.go og alt kjøre test

--- conv/conv.go
+++ conv/conv.go
@@ -21,30 +21,40 @@ func FarhenheitToCelsius(value float64) float64 {
 	// Du skal ikke formattere float64 i denne funksjonen
 	// Gjør formattering i main.go med fmt.Printf eller
 	// lignende
-	celsius := (value - 32) * (5.0 / 9.0)
+	convValue := (value - 32) * (5.0 / 9.0)
 
-	return math.Round(celsius*100) / 100
+	return math.Round(convValue*100) / 100
 }
 
 // De andre konverteringsfunksjonene implementere her
 // ...
 
 func FarhenheitToKelvin(value float64) float64 {
-	return 0
+	convValue := (value-32)*(5.0/9.0) + 273.15
+
+	return math.Round(convValue*100) / 100
 }
 
 func CelsiusToFahrenheit(value float64) float64 {
-	return 0
+	convValue := value*(9.0/5.0) + 32
+
+	return math.Round(convValue*100) / 100
 }
 
 func CelsiusToKevin(value float64) float64 {
-	return 0
+	convValue := value + 273.15
+
+	return math.Round(convValue*100) / 100
 }
 
 func KelvinToFarhenheit(value float64) float64 {
-	return 0
+	convValue := (value-273.15)*(9.0/5.0) + 32
+
+	return math.Round(convValue*100) / 100
 }
 
 func KevinToCelsius(value float64) float64 {
-	return 0
+	convValue := value - 273.15
+
+	return math.Round(convValue*100) / 100
 }
